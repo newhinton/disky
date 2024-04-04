5ad98b28110cb5c2721e9820c8d4d2aec46c4de0
Felix Nüsse <felix.nuesse@t-online.de>
2024-04-04 09:30:29 +0200

    propery convert units

--- app/src/main/java/de/felixnuesse/disky/extensions/fs.kt
+++ app/src/main/java/de/felixnuesse/disky/extensions/fs.kt
@@ -8,6 +8,6 @@ import kotlin.math.pow
 fun Any.readableFileSize(size: Long): String {
     if (size <= 0) return "0"
     val units = arrayOf("B", "kB", "MB", "GB", "TB")
-    val digitGroups = (log10(size.toDouble()) / log10(1024.0)).toInt()
-    return DecimalFormat("#,##0.#").format(size / 1024.0.pow(digitGroups.toDouble())) + " " + units[digitGroups]
+    val digitGroups = (log10(size.toDouble()) / log10(1000.0)).toInt()
+    return DecimalFormat("#,##0.#").format(size / 1000.0.pow(digitGroups.toDouble())) + " " + units[digitGroups]
 }
