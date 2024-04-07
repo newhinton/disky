822a2fd4313f845221b665ffc35f0482972371ec
Felix Nüsse <felix.nuesse@t-online.de>
2024-04-07 16:24:37 +0200

    update version to semver

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 30
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
