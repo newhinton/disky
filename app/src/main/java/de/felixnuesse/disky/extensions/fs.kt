a040f020c0d1af35776aed755af3f60333ace5b3
Felix Nüsse <felix.nuesse@t-online.de>
2024-04-07 18:36:25 +0200

    rename filesize

--- app/src/main/java/de/felixnuesse/disky/extensions/fs.kt
+++ app/src/main/java/de/felixnuesse/disky/extensions/fs.kt
@@ -7,7 +7,7 @@ import kotlin.math.pow
 //https://stackoverflow.com/a/5599842
 fun Any.readableFileSize(size: Long): String {
     if (size <= 0) return "0"
-    val units = arrayOf("B", "kB", "MB", "GB", "TB")
+    val units = arrayOf("B", "KB", "MB", "GB", "TB")
     val digitGroups = (log10(size.toDouble()) / log10(1000.0)).toInt()
     return DecimalFormat("#,##0.#").format(size / 1000.0.pow(digitGroups.toDouble())) + " " + units[digitGroups]
 }
