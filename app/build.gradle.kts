28e9b8af86cbaa1119b10b8b9e3fbc0899259b7e
Felix Nüsse <felix.nuesse@t-online.de>
2025-05-04 20:11:51 +0200

    use better color for donate button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "de.felixnuesse.disky"
         minSdk = 30
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.2.0"
+        versionCode = 8
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
